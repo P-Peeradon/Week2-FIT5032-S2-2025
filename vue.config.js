ab3d3f3c7d32a3e9a815f54e5cde11c647191117
P-Peeradon <p.determinant.integrate3@gmail.com>
2025-10-24 13:10:50 +1100

    Changing path

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/P-Peeradon/Week2-FIT5032-S2-2025/' : '/',
+  publicPath:
+    process.env.NODE_ENV === 'production' ? '/P-Peeradon.github.io/Week2-FIT5032-S2-2025/' : '/',
 }
