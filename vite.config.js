bceccf27f5e657d92beef8309cc836f93ac7c745
P-Peeradon <p.determinant.integrate3@gmail.com>
2025-10-24 14:05:21 +1100

    Vite config

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+const BASE_PATH = '/Week2-FIT5032-S2-2025/' // Use your repository name here
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
