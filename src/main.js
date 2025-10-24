14346f686da9a475412fffc605befa1ac21d862e
P-Peeradon <p.determinant.integrate3@gmail.com>
2025-10-24 13:54:19 +1100

    Add relative path

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router/index'
 
 // import bootstrap css
 import 'bootstrap/dist/css/bootstrap.min.css'
